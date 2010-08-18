0787915fec0e9ecb2130ca8d364ffa77983ebdd8
Adrian Likins <adrian@adrian-mac-2.local>
2010-08-18 12:18:08 -0400

    Add support for logging battery voltage as well.
    Open and close the log file on every image. Was running into
    cases where sometimes the file wouldn't be written if interupted,
    even with a flush().

--- mint.lua
+++ mint.lua
@@ -3,24 +3,31 @@
 @param a Shooting interval, min
 @default a 0
 @param b ...sec
-@default b 10
+@default b 15
 --]]
  
 Interval = a*60000 + b*1000
-logfile=io.open("A/paramdmp.log","wb")
+--logfile=io.open("A/baloon_temp.log","wb")
  
 function TakePicture()
+	logfile=io.open("A/baloon_temp.log", "ab")
+	logfile:seek("end")
 	press("shoot_half")
         repeat sleep(50) until get_shooting() == true
 	press("shoot_full")
 	release("shoot_full")
 	repeat sleep(50) until get_shooting() == false	
         release "shoot_half"
-	a = get_temperature(0)
-	b = get_temperature(1)
-	c = get_temperature(2)
-	logfile:write(string.format("%s %d %d %d\n", os.time(), a,b,c))
+	opt_temp = get_temperature(0)
+	ccd_temp = get_temperature(1)
+	batt_temp = get_temperature(2)
+	-- write out vaguely csv style
+	batt_voltage = get_vbatt()
+	logfile:write(string.format("%s,%d,%d,%d,%d\n", os.time(), opt_temp,ccd_temp,batt_temp,batt_voltage))
 	logfile:flush()
+	logfile:close()
+	-- probably need a delay here, but using a jack in the av port
+	-- is probably even better battery wise
 	set_backlight(0)
 end
  
@@ -29,4 +36,3 @@ repeat
 	TakePicture()
 	sleep(Interval - (get_tick_count() - StartTick))
 until false
-logfile:close()
